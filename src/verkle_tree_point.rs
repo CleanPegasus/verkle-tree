6be2e45bf4b355ae384e10e13fdfdb0365b80adc
TjitskeKoster <t.o.koster@tudelft.nl>
2025-05-21 09:13:07 +0200

    update verkle tree to be 1 layer

--- src/verkle_tree_point.rs
+++ src/verkle_tree_point.rs
@@ -252,14 +252,15 @@ impl VerkleTree {
         }
         // We need to add the root manually
         //println!("{:?}", tree_path);
-        println!("depth {}", depth);
-        let mut node_root: Vec<usize> = Vec::new();
-        for child in 0.. width{
-            if !tree_path[tree_path.len()- child-1].is_empty(){
-                node_root.push(child);
+        if depth != 0 {
+            let mut node_root: Vec<usize> = Vec::new();
+            for child in 0.. width{
+                if !tree_path[tree_path.len()- child-1].is_empty(){
+                    node_root.push(child);
+                }
             }
+            tree_path.push( node_root);
         }
-        tree_path.push( node_root);
         tree_path.reverse();
         //println!("tree proofs {:?}", tree_proofs);
         //println!("tree path{:?}", tree_path);
